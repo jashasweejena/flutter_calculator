f57fe4e8831779eef93713eb22d57c890856c8ab
Jashaswee Jena <j.ishujena2000@gmail.com>
2018-10-20 18:18:44 +0530

    initial

---
+++ lib/home_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  State createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+  var num1 = 0, num2 = 0, sum = 0;
+
+  TextEditingController t1 = new TextEditingController(text: "0");
+  TextEditingController t2 = new TextEditingController(text: "0");
+
+  void _addNumbers(){
+    setState(() {
+          num1 = int.parse(t1.text);
+          num2 = int.parse(t2.text);
+          sum = num1 + num2;
+          t1 = new TextEditingController(text: sum.toString());
+          t2 = new TextEditingController(text: "0");
+        });
+  }
+
+  void _diffNumbers(){
+    setState(() {
+          num1 = int.parse(t1.text);
+          num2 = int.parse(t2.text);
+          sum = (num1 - num2).abs();
+          t1 = new TextEditingController(text: sum.toString());
+          t2 = new TextEditingController(text: "0");
+        });
+  }
+void _multiplyNumbers(){
+    setState(() {
+          num1 = int.parse(t1.text);
+          num2 = int.parse(t2.text);
+          sum = num1 * num2;
+          t1 = new TextEditingController(text: sum.toString());
+          t2 = new TextEditingController(text: "0");
+        });
+  }
+void _divideNumbers(){
+    setState(() {
+          num1 = int.parse(t1.text);
+          num2 = int.parse(t2.text);
+
+          if(num2 != 0){
+          sum = num1 ~/ num2;
+          }
+          else{
+            sum = 0;
+          } //To avoid division by zero exception.
+          t1 = new TextEditingController(text: sum.toString());
+          t2 = new TextEditingController(text: "0");
+        });
+  }
+
+  void _resetAll(){
+    setState(() {
+          t1 = new TextEditingController(text: "0");
+          t2 = new TextEditingController(text: "0");
+          sum = 0;
+        });
+  }
+  Widget _bodyWidget(){
+    return new Container(
+      padding: const EdgeInsets.all(40.0),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text(
+            "Output: $sum",
+            style: new TextStyle(
+              fontSize: 20.0,
+              color: Colors.purple,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          new Padding(padding: EdgeInsets.only(bottom: 50.0)),
+          new TextField(
+            keyboardType: TextInputType.number,
+            decoration: new InputDecoration(
+              hintText: "Enter number 1"
+            ),
+            controller: t1,
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(bottom: 30.0)
+          ),
+          new TextField(
+            keyboardType: TextInputType.number,
+            decoration: new InputDecoration(
+              hintText: "Enter number 2"
+            ),
+            controller: t2,
+          ),
+          new Padding(padding: EdgeInsets.only(bottom: 30.0)),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new MaterialButton(
+                child: new Text("+"),
+                onPressed: _addNumbers,
+                color: Colors.greenAccent,
+              ),
+              new MaterialButton(
+                child: new Text("-"),
+                onPressed: _diffNumbers,
+                color: Colors.greenAccent,
+              ),
+            ],
+          ),
+          new Padding(padding: EdgeInsets.only(bottom: 10.0),),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new MaterialButton(
+                child: new Text("*"),
+                onPressed: _multiplyNumbers,
+                color: Colors.greenAccent,
+              ),
+              new MaterialButton(
+                child: new Text("/"),
+                onPressed: _divideNumbers,
+                color: Colors.greenAccent,
+                splashColor: Colors.blue,
+              ),
+            ],
+          ),
+        ],
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Calculator"),
+      ),
+      body: _bodyWidget(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.restore),
+        elevation: 50.0,
+            onPressed: _resetAll,
+      ),
+    );
+  }
+}
